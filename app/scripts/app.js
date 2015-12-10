d944f12c7dc90e1881c0e86aa787ee6e901ad9e7
Daniel Madera <madera.dan@gmail.com>
2015-12-10 23:32:26 +0100

    Modified ajax to ngResource

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,25 +8,42 @@
  *
  * Main module of the application.
  */
-var publicDebate = angular.module('publicDebate', [
-        'ngAnimate',
-        'ngCookies',
-        'ngResource',
-        'ngRoute'
-    ])
-    .config(function($routeProvider) {
-        $routeProvider
-            .when('/', {
-                templateUrl: 'views/threads.html',
-                controller: 'threadsController',
-                controllerAs: 'threads'
-            })
-            .when('/about', {
-                templateUrl: 'views/about.html',
-                controller: 'aboutController',
-                controllerAs: 'about'
-            })
-            .otherwise({
-                redirectTo: '/'
-            });
-    });
+var app = angular.module('publicDebate', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute'
+]);
+
+var server = "http://private-54742-pwa2015publicdebate.apiary-mock.com/pwa2015publicdebate";
+
+app.factory('Threads', ['$resource', function($resource) {
+    return $resource(server + '/threads', {});
+}]);
+
+app.config(function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'views/threads-list.html',
+            controller: 'threadsController',
+            controllerAs: 'threads'
+        })
+        .when('/threads', {
+            templateUrl: 'views/threads-list.html',
+            controller: 'threadsController',
+            controllerAs: 'threads'
+        })
+        .when('/threads/:thread_id', {
+            templateUrl: 'views/thread-detail.html',
+            controller: 'threadController',
+            controllerAs: 'thread'
+        })
+        .when('/about', {
+            templateUrl: 'views/about.html',
+            controller: 'aboutController',
+            controllerAs: 'about'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+});
