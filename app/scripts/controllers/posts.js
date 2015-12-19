ed8ae670c55a3f9e02aedbdc9785ac1ae14d83b1
Daniel Madera <madera.dan@gmail.com>
2015-12-19 19:37:15 +0100

    Removed unused console logsx

---
+++ app/scripts/controllers/posts.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var app = angular.module('publicDebate');
+
+app.controller('PostsController', function($scope, $routeParams, PostsResource, PaginationService) {
+    $scope.thread_id = $routeParams.thread_id;
+
+    $scope.get = function() {
+        var params = {
+            thread_id: $scope.thread_id
+        }
+
+        var success = function(value, responseHeaders) {
+            $scope.postsObject = value;
+            angular.extend($scope.postsObject, {
+                pagination: PaginationService.getPagination(responseHeaders)
+            });
+        }
+
+        var error = function(httpResponse) {
+            console.log(httpResponse);
+        }
+
+        PostsResource.get(params, success, error);
+    }
+
+    $scope.create = function() {
+        if ($scope.text == '') {
+            return false;
+        }
+
+        var postData = {
+            text: $scope.text
+        };
+
+        var success = function(value, responseHeaders) {
+            $scope.text = '';
+            $scope.get();
+        }
+
+        var error = function(httpResponse) {
+            console.log(httpResponse);
+        }
+
+        PostsResource.save({
+            thread_id: $scope.thread_id
+        }, postData, success, error);
+    };
+
+    angular.element(document).ready(function() {
+        $scope.get();
+    });
+});
